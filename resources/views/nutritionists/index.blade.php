45c82f070972f0d3feeeab23d8c7af6d50ad9835
Asumi <>
2025-01-02 12:36:22 +0800

    I made sure that if a nutritionist could see his or her users

--- resources/views/nutritionists/index.blade.php
+++ resources/views/nutritionists/index.blade.php
@@ -1,84 +1,37 @@
 @extends('layouts.app')
+
 @section('title', 'Home')
+
 @section('content')
-
-
 <div class="container w-75">
-    <h3 class="pt-5 pb-2">Send Feedback to</h2>
+    <h3 class="pt-5 pb-2 nutri-h2">Send Feedback to</h3>
     <div class="row g-4">
-      <!-- 8名分のカードをループして作成 -->
-      <div class="col-12 col-sm-6 col-md-3 col-lg-3">
-        <div class="card nutri-card">
-          <img src="https://via.placeholder.com/80" class="nutri-card-img-top " alt="Avatar">
-          <div class="card-body">
-            <h5 class="nutri-card-title">James Anderson</h5>
-            <button class="nutri-btn">Send Advice</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-sm-6 col-md-3 col-lg-3">
-        <div class="card nutri-card">
-          <img src="https://via.placeholder.com/80" class="nutri-card-img-top " alt="Avatar">
-          <div class="card-body">
-            <h5 class="nutri-card-title">Michael Carter</h5>
-            <button class="nutri-btn">Send Advice</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-sm-6 col-md-3 col-lg-3">
-        <div class="card nutri-card">
-          <img src="https://via.placeholder.com/80" class="nutri-card-img-top " alt="Avatar">
-          <div class="card-body">
-            <h5 class="nutri-card-title">David Thompson</h5>
-            <button class="nutri-btn">Send Advice</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-sm-6 col-md-3 col-lg-3">
-        <div class="card nutri-card">
-          <img src="https://via.placeholder.com/80" class="nutri-card-img-top " alt="Avatar">
-          <div class="card-body">
-            <h5 class="nutri-card-title">John Walker</h5>
-            <button class="nutri-btn">Send Advice</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-sm-6 col-md-3 col-lg-3">
-        <div class="card nutri-card">
-          <img src="https://via.placeholder.com/80" class="nutri-card-img-top " alt="Avatar">
-          <div class="card-body">
-            <h5 class="nutri-card-title">Robert Parker</h5>
-            <button class="nutri-btn">Send Advice</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-sm-6 col-md-3 col-lg-3">
-        <div class="card nutri-card">
-          <img src="https://via.placeholder.com/80" class="nutri-card-img-top " alt="Avatar">
-          <div class="card-body">
-            <h5 class="nutri-card-title">Jordan Bailey</h5>
-            <button class="nutri-btn">Send Advice</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-sm-6 col-md-3 col-lg-3">
-        <div class="card nutri-card">
-          <img src="https://via.placeholder.com/80" class="nutri-card-img-top " alt="Avatar">
-          <div class="card-body">
-            <h5 class="nutri-card-title">Jessica Brown</h5>
-            <button class="nutri-btn">Send Advice</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-sm-6 col-md-3 col-lg-3">
-        <div class="card nutri-card">
-          <img src="https://via.placeholder.com/80" class="nutri-card-img-top " alt="Avatar">
-          <div class="card-body">
-            <h5 class="nutri-card-title">Sophia Miller</h5>
-            <button class="nutri-btn">Send Advice</button>
-          </div>
-        </div>
-      </div>
+        @forelse ($users as $user)
+            <div class="col-12 col-sm-6 col-md-3 col-lg-3">
+                <div class="card nutri-card">
+                    <!-- ユーザーのアバター画像表示 -->
+                    @if ($user->avatar)
+                        <img src="{{ $user->avatar }}" class="nutri-card-img-top" alt="Avatar">
+                    @else
+                        <span class="material-symbols-outlined">
+                        account_circle
+                    </span>
+                    @endif
+                    <div class="card-body">
+                        <!-- ユーザー名 -->
+                        <h5 class="nutri-card-title">{{ $user->name }}</h5>
+
+                        <!-- Send Advice Button with Form for Submission -->
+                        <form action="{{ route('nutri.sendAdvice', $user->id) }}" method="POST">
+                            @csrf
+                            <button type="submit" class="nutri-btn">Send Advice</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <div>No Task</div>
+        @endforelse
     </div>
-  </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/nutri/index', [NutritionistController::class, 'index']);
-Route::get('nutri/sendAdvice', [NutritionistController::class, 'sendAdvice']);
-Route::get('nutri/history', [NutritionistController::class, 'history']);
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::group(['prefix' => 'nutri', 'as' => 'nutri.'], function(){
+
+        Route::get('nutri/sendAdvice', [NutritionistController::class, 'sendAdvice'])->name('sendAdvice');
+        Route::get('nutri/history', [NutritionistController::class, 'history']);
+    });
+});
+
+Route::get('/nutri/index', [NutritionistController::class, 'index'])->name('index');
 
 
 //user dailylog
