cb2ec5986ba179a6b62c7d193cf7eaa2d07e7a43
MSI\abaoa <a.baoaku00@gmail.com>
2025-01-08 13:23:16 +0900

    もうよくわからん

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -26,8 +26,7 @@
         <a class="navbar-brand" href="#">FITTY</a>
         <div>
             <a class="nav-link login" href="{{ route('login') }}">Login</a>
-            <a class="nav-link" href="{{ route('about') }}">About Us</a>
-            <a class="nav-link" href="#">About Us</a>
+            <a class="nav-link" href="{{route('about')}}">About Us</a>
         </div>
     </nav>
 
