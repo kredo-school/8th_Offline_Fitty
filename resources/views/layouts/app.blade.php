b4fb7bb2fccebe7042580aa4de089ed1a6ad7b6a
Asumi <>
2025-01-15 10:00:35 +0800

    small changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,9 +66,6 @@
                     <img src="{{ asset('images/fitty_logo.png') }}" width=60px;  alt="logo">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
