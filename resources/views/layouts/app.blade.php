89368a28906cac7eed9ed60b893e68ce4b7efb0b
今井絵美子 <imyrin0512@gmail.com>
2025-01-15 13:25:35 +0900

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,9 +19,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Shrikhand&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-    {{-- <link href="{{ asset('css/admin/style.css') }}" rel="stylesheet"> 
-    @stack('styles') --}}
-
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
 
