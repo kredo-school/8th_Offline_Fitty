c475962d1a995ec239af00359c1a861a569e5b4e
今井絵美子 <imyrin0512@gmail.com>
2025-01-15 13:04:39 +0900

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Shrikhand&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-    <link href="{{ asset('css/admin/style.css') }}" rel="stylesheet"> 
-    @stack('styles')
+    {{-- <link href="{{ asset('css/admin/style.css') }}" rel="stylesheet"> 
+    @stack('styles') --}}
 
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
@@ -135,6 +135,6 @@
 
 
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -36,28 +36,20 @@ Auth::routes();
 
 // Adminルート
 Route::prefix('admin')->name('admin.')->group(function () {
-
-    // // 認証関連のルート
-    // Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
-    // Route::post('login', [AuthController::class, 'login']);
-    // Route::get('password/request', [AuthController::class, 'showForgotPasswordForm'])->name('password.request');
-
-    // Admin Index
     Route::get('/index', [AdminController::class, 'index'])->name('index');
-
-    // リソース別ルート
     Route::get('/users', [UsersController::class, 'index'])->name('users.index');
     Route::delete('/users/{user}', [UsersController::class, 'destroy'])->name('users.destroy');
 
     Route::get('/nutritionists', [NutritionistsController::class, 'index'])->name('nutritionists.index');
-    Route::get('/inquiries', [InquiriesController::class, 'index'])->name('inquiries.index');
+    Route::get('/nutritionists/create', [NutritionistsController::class, 'create'])->name('nutritionists.create');
 
-    // Categories関連のルート
+    Route::get('/inquiries', [InquiriesController::class, 'index'])->name('inquiries.index');
     Route::get('/categories', [CategoriesController::class, 'index'])->name('categories.index');
     Route::post('/categories', [CategoriesController::class, 'store'])->name('categories.store');
     Route::put('/categories/{id}', [CategoriesController::class, 'update'])->name('categories.update');
     Route::delete('/categories/{id}', [CategoriesController::class, 'destroy'])->name('categories.destroy');
-}); 
+});
+
 
 Route::get('/about', function () {
     return view('about');
