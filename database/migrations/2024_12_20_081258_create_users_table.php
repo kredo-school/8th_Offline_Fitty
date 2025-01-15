734cf0082b73d39d43c0164429828ca105b174e0
Atsuki <omorichess0808@gmail.com>
2025-01-15 11:45:09 +0800

    create seeder files for dailylog

--- database/migrations/2024_12_20_081258_create_users_table.php
+++ database/migrations/2024_12_20_081258_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->longtext('avatar')->nullable();
             $table->string('role')->default('U');
             $table->timestamps();
         });
