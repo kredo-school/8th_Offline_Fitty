cbc41ba9625dce09ba1474253183e32e244848fb
Ryohei Oishi <trop4454175@gmail.com>
2025-01-16 09:17:23 +0800

    Fix login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,32 +32,6 @@
 </head>
 <body>
     <div id="app">
-       {{-- @if(auth()->check())
-        <!-- サイドメニュー -->
-        <div class="sidemenu">
-            <a href="#" class="menu-item">
-                <span class="material-icons mt-3">assignment</span> Record
-            </a>
-            <a href="#" class="menu-item">
-                <span class="material-icons">person</span> Profile
-            </a>
-            <a href="#" class="menu-item">
-                <span class="material-icons">history</span> History
-            </a>
-            <a href="#" class="menu-item active">
-                <span class="material-icons">notifications</span> Notification
-            </a>
-            <a href="#" class="menu-item">
-                <span class="material-icons">help_outline</span> Help
-            </a>
-            <a href="#" class="menu-item">
-                <span class="material-icons">help</span> FAQ
-            </a>
-            <a href="#" class="menu-item">
-                <span class="material-icons">logout</span> Logout
-            </a>
-        </div>
-        @endif --}}
 
         <!-- ナビバー(header) -->
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm fixed-top">
