d198d3b3a80a681dcceb973f1e4911072abafe22
Atsuki <omorichess0808@gmail.com>
2025-01-16 09:12:51 +0800

    とりあえずコミット

--- resources/views/users/inputmeal.blade.php
+++ resources/views/users/inputmeal.blade.php
@@ -13,13 +13,13 @@
         <!-- Date Input -->
         <div class="mb-3">
             <label for="date" class="form-label">Date</label>
-            <input type="date" class="form-control" id="date" value="2024-12-14">
+            <input type="date" name="input_date" class="form-control" id="date" value="2024-12-14">
         </div>
 
         <!-- Meal Type Dropdown -->
         <div class="mb-3">
             <label for="mealType" class="form-label">Meal Type</label>
-            <select class="form-select" id="mealType">
+            <select class="form-select" id="mealType" name="meal_type">
                 <option selected>Breakfast</option>
                 <option value="1">Lunch</option>
                 <option value="2">Dinner</option>
@@ -30,7 +30,7 @@
        <!-- Comment Textarea -->
        <div class="mb-3">
             <label for="comment" class="form-label">Meal</label>
-            <textarea class="form-control textarea-border" id="comment" rows="3" placeholder="Write here"></textarea>
+            <textarea class="form-control textarea-border" name="meal_content" id="comment" rows="3" placeholder="Write here"></textarea>
         </div>
 
         <!-- Comment Textarea -->
