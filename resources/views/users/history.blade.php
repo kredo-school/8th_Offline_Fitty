f0078b1b4a12f85a45b3aa60ae4e10ca2c904144
Atsuki <omorichess0808@gmail.com>
2025-01-27 12:31:45 +0800

    fix sidebar

--- resources/views/users/history.blade.php
+++ resources/views/users/history.blade.php
@@ -5,17 +5,12 @@
 @section('content')
 
 @include('sidebar.humburger')
-<div class="container user-history">
+<div class="row main-row">
     <!-- Header Section -->
-    <div class="user-history-header">
-        <div class="row">
-            @include('sidebar.user-sidebar') 
-            <div class="col-md-9 ms-sm-auto col-lg-10"> 
-            <!-- main content -->
-             <h1 class="text-center">History</h1>
-             
-            </div>
-        </div>
+    @include('sidebar.user-sidebar') 
+    <div class="col-md-9 ms-sm-auto col-lg-10 user-history"> 
+        <!-- main content -->
+        <h1 class="text-center">History</h1>             
     </div>
 </div>
 
