50e1f8fab74ce4aa20978606116667189383ada6
Atsuki <omorichess0808@gmail.com>
2025-02-11 11:25:01 +0800

    Make advice seeder file

--- database/seeders/AdviceSeeder.php
+++ database/seeders/AdviceSeeder.php
@@ -13,17 +13,19 @@ class AdviceSeeder extends Seeder
         $advices = [];
 
         // ユーザー情報を取得
-        $users = User::where('role', 'U')->get();
+        $users = User::where('role', 'U')->take(2)->get();
 
         foreach ($users as $user) {
-            $advices[] = [
-                'overall' => rand(1, 5), // ランダムな評価（1〜5）
-                'message' => 'Advice message for user ' . $user->id, // 仮のアドバイス内容
-                'user_id' => $user->user_profile->user_id + 1, // ユーザーIDを紐付け
-                'nutritionist_id' => $user->user_profile->nutritionist_id + 51, // ユーザーIDを紐付け
-                'created_at' => now()->subDays(rand(0, 30)), // 過去30日以内のランダムな日付
-                'updated_at' => now(),
-            ];
+            for ($i = 1; $i <= 100; $i++) {
+                $advices[] = [
+                    'overall' => rand(1, 5), // ランダムな評価（1〜5）
+                    'message' => 'Advice message for user ' . $user->id, // 仮のアドバイス内容
+                    'user_id' => $user->id, // ユーザーIDを紐付け
+                    'nutritionist_id' => $user->id + 50, // ユーザーIDを紐付け
+                    'created_at' => now()->subDays(rand(0, 15)), // 過去30日以内のランダムな日付
+                    'updated_at' => now(),
+                ];
+            }
         }
 
         // データを一括挿入
